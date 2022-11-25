e6fbfbde91d972dcde577b8828b3c04d09a53f89
Negar Tolou <negart@energizeglobal.com>
2022-11-25 12:26:22 +0400

    edit- edited the initail page of application

---
+++ Moviedb/View/MoviedbApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviedbApp.swift
+//  Moviedb
+//
+//  Created by Negar Tolou on 25.11.22.
+//
+
+import SwiftUI
+
+@main
+struct MoviedbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MoviesListView()
+        }
+    }
+}
