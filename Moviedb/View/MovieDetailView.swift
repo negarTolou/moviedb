642685a1025b2557e380c5ebf01e1c78ea081f32
Negar Tolou <negart@energizeglobal.com>
2022-11-26 20:10:29 +0400

    add- added movieDetailView

---
+++ Moviedb/View/MovieDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieDetailView.swift
+//  Moviedb
+//
+//  Created by Negar Tolou on 25.11.22.
+//
+
+import SwiftUI
+
+struct MovieDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieDetailView()
+    }
+}
