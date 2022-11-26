2ec54c2faec929aae5ef82d30f8064e10ce624e9
Negar Tolou <negart@energizeglobal.com>
2022-11-26 20:10:12 +0400

    add- added urlImageView ui and bind with viewModel

---
+++ Moviedb/View/UrlImageView.swift
@@ -0,0 +1,32 @@
+//
+//  UrlImageView.swift
+//  Moviedb
+//
+//  Created by Negar Tolou on 25.11.22.
+//
+
+import Foundation
+import SwiftUI
+
+struct UrlImageView: View {
+    @ObservedObject var urlImageVM: UrlImageViewModel
+    
+    init(urlString: String?) {
+        urlImageVM = UrlImageViewModel(urlString: urlString)
+    }
+    
+    var body: some View {
+        Image(uiImage: urlImageVM.image ?? UrlImageView.defaultImage!)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 300, height: 300)
+    }
+    
+    static var defaultImage = UIImage(named: "logo")
+}
+
+struct UrlImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        UrlImageView(urlString: nil)
+    }
+}
