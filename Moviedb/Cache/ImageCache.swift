652a7d4310f66a1444adc8950fe07f83fb18070d
Negar Tolou <negart@energizeglobal.com>
2022-11-26 20:08:05 +0400

    add- added NSCache

---
+++ Moviedb/Cache/ImageCache.swift
@@ -0,0 +1,28 @@
+//
+//  ImageCache.swift
+//  Moviedb
+//
+//  Created by Negar Tolou on 25.11.22.
+//
+
+import Foundation
+import UIKit
+
+class ImageCache {
+    var cache = NSCache<NSString, UIImage>()
+    
+    func get(forKey: String) -> UIImage? {
+        return cache.object(forKey: NSString(string: forKey))
+    }
+    
+    func set(forKey: String, image: UIImage) {
+        cache.setObject(image, forKey: NSString(string: forKey))
+    }
+}
+
+extension ImageCache {
+    private static var imageCache = ImageCache()
+    static func getImageCache() -> ImageCache {
+        return imageCache
+    }
+}
