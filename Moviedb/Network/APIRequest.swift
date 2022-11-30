94d5171f035d577a860ebbbd2d900758f9f53c19
Negar Tolou <negart@energizeglobal.com>
2022-11-30 13:17:25 +0400

    add- added API request- protocol API - set some constants  for baseUrl and acceptable statusCode range

---
+++ Moviedb/Network/APIRequest.swift
@@ -0,0 +1,26 @@
+//
+//  APIRequest.swift
+//  Moviedb
+//
+//  Created by Negar Tolou on 29.11.22.
+//
+
+import Foundation
+protocol API {
+    var path: String { get }
+    var baseURL: String { get }
+    var header: [String: String] { get }
+    var method: URLRequestMethod { get }
+    var queries: [String: String] { get }
+    var acceptableStatusCodes: ClosedRange<Int> { get }
+}
+
+extension API {
+    var baseURL: String {
+        return Constants.baseURL
+    }
+    var acceptableStatusCodes: ClosedRange<Int> {
+        return 200...299
+    }
+    var queries: [String: String] { [:] }
+}
