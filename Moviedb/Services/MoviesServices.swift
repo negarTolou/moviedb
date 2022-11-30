7a5ee559100a7088e4ead5f864d0f56fcdd41bc3
Negar Tolou <negart@energizeglobal.com>
2022-11-30 14:54:19 +0400

    add- added movieService

---
+++ Moviedb/Services/MoviesServices.swift
@@ -0,0 +1,29 @@
+//
+//  MoviesServices.swift
+//  Moviedb
+//
+//  Created by Negar Tolou on 29.11.22.
+//
+
+import Foundation
+
+enum ServiceError: Error {
+    case invalideResponse
+}
+
+struct MovieListService {
+    let session: Sessionable
+    let parser: Parsable
+    func fetch(api: MovieListAPI) async throws -> PopularMovieResponseModel {
+        let response = try await session.request(builder: URLBuilder(api: api))
+        if let httpResponse = response.1 as? HTTPURLResponse, api.acceptableStatusCodes.contains(httpResponse.statusCode) {
+            return try parser.parse(data: response.0)
+            // Do parsing
+        }
+        throw ServiceError.invalideResponse
+    }
+}
+
+protocol Sessionable {
+    func request(builder: URLBuilder) async throws -> (Data, URLResponse)
+}
