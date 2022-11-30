fe283d088691c9bb1d526ba0105eeed49a8945c2
Negar Tolou <negart@energizeglobal.com>
2022-11-30 17:09:24 +0400

    edit- call LoadImage onApear

--- Moviedb/View/UrlImageView.swift
+++ Moviedb/View/UrlImageView.swift
@@ -10,16 +10,32 @@ import SwiftUI
 
 struct UrlImageView: View {
     @ObservedObject var urlImageVM: UrlImageViewModel
+
+    @State var image: UIImage!
+    var defaultImage = UIImage(named: "logo")
+    
     init(urlString: String?) {
         urlImageVM = UrlImageViewModel(urlString: urlString)
     }
+    
     var body: some View {
-        Image(uiImage: urlImageVM.image ?? UrlImageView.defaultImage!)
+        Image(uiImage: image ?? defaultImage!)
             .resizable()
             .scaledToFit()
             .frame(width: 300, height: 300)
+            .onAppear {
+               loadImage()
+            }
     }
-    static var defaultImage = UIImage(named: "logo")
+    
+    func loadImage() {
+        Task {
+            let asyncIMage = try? await urlImageVM.getImage()
+            DispatchQueue.main.async {
+                image = asyncIMage
+            }
+        }
+    }
 }
 
 struct UrlImageView_Previews: PreviewProvider {
