667ef59a9ab9c6614cd6298f7c69c906f2b5d07d
Negar Tolou <negart@energizeglobal.com>
2022-11-30 13:23:11 +0400

    add- added movieListAPI

---
+++ Moviedb/Network/APIs/MovieListAPI.swift
@@ -0,0 +1,20 @@
+//
+//  MovieListAPI.swift
+//  Moviedb
+//
+//  Created by Negar Tolou on 29.11.22.
+//
+
+import Foundation
+struct MovieListAPI: API {
+    var path: String {
+        return "3/movie/popular"
+    }
+    var baseURL: String = Constants.baseURL
+    var header: [String : String] = [:]
+    var method: URLRequestMethod = .get
+    
+    var queries: [String : String] {
+        return ["api_key": Constants.apiKey]
+    }
+}
