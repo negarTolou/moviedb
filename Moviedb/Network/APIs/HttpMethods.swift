1ddbed2200b7bef2685f90b57c89d4cd280d596f
Negar Tolou <negart@energizeglobal.com>
2022-11-30 13:19:47 +0400

    add- added enum of httpMethods

---
+++ Moviedb/Network/APIs/HttpMethods.swift
@@ -0,0 +1,13 @@
+//
+//  HttpMethods.swift
+//  Moviedb
+//
+//  Created by Negar Tolou on 30.11.22.
+//
+
+enum URLRequestMethod: String {
+    case get = "GET"
+    case put = "PUT"
+    case delete = "DELETE"
+    case post = "POST"
+}
