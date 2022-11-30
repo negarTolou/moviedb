d4205a13f1b29ff5f748c2ac7269034273aafc12
Negar Tolou <negart@energizeglobal.com>
2022-11-30 13:17:48 +0400

    add- added baseUrl and apiKey constants

---
+++ Moviedb/Utility/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Moviedb
+//
+//  Created by Negar Tolou on 29.11.22.
+//
+
+import Foundation
+
+struct Constants {
+    static let baseURL = "https://api.themoviedb.org"
+    static let apiKey = "db19d1f9fd84d47d6e26e13d2d97e21a"
+ }
+
